8a0404a7e0f985ed7751b4506ddcec3cb7fcb6f5
nworb9 <emma.nworb@gmail.com>
2023-06-04 19:52:34 -0400

    pair product

---
+++ chap1/pair-product.cpp
@@ -0,0 +1,15 @@
+#include <array>
+#include <vector>
+#include <unordered_map>
+
+std::array<int, 2> pairProduct(std::vector<int> numbers, int target) {
+  std::unordered_map<int, int> previousNums;
+  for (int i = 0; i < numbers.size(); i += 1) {
+    int complement = target / numbers[i];
+    if (previousNums.count(complement) > 0) {
+      return std::array<int, 2> { i, previousNums[complement] };
+    }
+    previousNums[numbers[i]] = i;
+  }
+}
+
